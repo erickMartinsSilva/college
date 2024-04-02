17c78c1794a5d316a620ce6b42862f95bb27fc77
Erick Martins <erickmsilva19@gmail.com>
2024-04-02 09:19:24 -0300

    Update eq2g.c

--- c/eq2g.c
+++ c/eq2g.c
@@ -1,16 +1,15 @@
 // Nome: Erick Martins Silva
-// Data: 22/02/24
-// Instituição: CEFET/RJ
+// Instituição: Cefet/RJ
+// Período: 1
 // Descrição: O programa abaixo resolve uma equação do segundo grau utilizando a fórmula de Bhaskara
-
-#include <stdio.h> // biblioteca padrão do C com comandos de entrada e saída
-#include <math.h> // biblioteca para comandos de operações matemáticas como potenciação e radiciação
+#include <stdio.h>
+#include <math.h>
 
 int main() {
-    float a, b, c, delta, x1, x2, x; // declaração de variáveis do tipo "real"
-    printf("Digite valores para A, B e C: "); // printf faz a máquina escrever algo no terminal
-    scanf("%f %f %f", &a, &b, &c); // scanf lê o que o usuário escrever
-    delta = b*b - 4*a*c; // inserção de uma expressão matemática dentro da variável delta
+    float a, b, c, delta, x1, x2, x; 
+    printf("Digite valores para A, B e C: ");
+    scanf("%f %f %f", &a, &b, &c); 
+    delta = b*b - 4*a*c;
     if (delta > 0) {
         x1 = (-b + sqrt(delta)) / (2*a);
         x2 = (-b - sqrt(delta)) / (2*a);
