5629af9d45340a3778a4a3140aaf8b5d0a379baf
Erick Martins <erickmsilva19@gmail.com>
2024-10-16 14:05:52 -0300

    add wcat.c

---
+++ so/projeto1/wcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char buffer[100];
+    FILE *arquivo;
+    if(argc == 1) {
+        printf("Uso: ./wcat <nome_do_arquivo>");
+        return 1;
+    }
+    else {
+        for(int i = 1; i < argc; i++) {
+            arquivo = fopen(argv[i], "r");
+            if(arquivo == NULL) {
+                printf("wcat: cannot open file\n");
+                return 1;
+            }
+            else {
+                while(fgets(buffer, sizeof(buffer), arquivo) != NULL) {
+                    printf("%s", buffer);
+                }
+                printf("\n");
+            }
+            fclose(arquivo);
+        }
+    }
+    return 0;
+}
