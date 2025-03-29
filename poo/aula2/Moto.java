02f6219eaecd9d4c9348884ed75830e07e2720b6
Erick Martins <erickmsilva19@gmail.com>
2025-03-29 00:14:17 +0000

    feat: add class 2 practice

---
+++ poo/aula2/Moto.java
@@ -0,0 +1,9 @@
+public class Moto {
+    String placa;
+    String cor;
+    int ano;
+
+    public void ligar() {
+        System.out.println("moto " + placa + " ligando");
+    }
+}
---
+++ poo/aula2/SegundoPrograma.java
@@ -0,0 +1,30 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        System.out.println("ola");
+        Carro fiatStrada = new Carro();
+        fiatStrada.ano = 2006;
+        fiatStrada.cor = "preto";
+        System.out.println(fiatStrada.ano);
+        System.out.println(fiatStrada.cor);
+
+        Carro renaultKwid = fiatStrada;
+        renaultKwid.ano = 2023;
+        fiatStrada = new Carro();
+        fiatStrada.ano = 1999;
+        System.out.println(renaultKwid.ano);
+        System.out.println(fiatStrada.ano);
+
+        Moto cb1000 = new Moto();
+        cb1000.placa = "SAGJGIA";
+        cb1000.ano = 2001;
+        cb1000.cor = "verde";
+        System.out.println(cb1000.placa);
+        System.out.println(cb1000.ano);
+        System.out.println(cb1000.cor);
+
+        cb1000.ligar();
+
+        Moto m2 = null;
+        System.out.println(m2.placa);
+    }
+}
