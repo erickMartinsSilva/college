b09d021a3d6a7b93aa97486662a7e75340d8f0b8
Erick Martins <erickmsilva19@gmail.com>
2025-05-09 21:39:36 +0000

    poo aula 4

---
+++ poo/aula4/Conta.java
@@ -0,0 +1,50 @@
+public class Conta {
+    private String nomeCliente;
+    private int matricula;
+    private double saldo;
+    private String senha;
+
+    public String getNomeCliente() {
+        return this.nomeCliente;
+    }
+
+    public void setNomeCliente(String n) {
+        this.nomeCliente = n;
+    }
+
+    public String getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double s) {
+        this.saldo = s;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String s) {
+        this.senha = s;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int m) {
+        this.matricula = m;
+    }
+
+    public void debitar(double valor) {
+        if(valor > this.saldo) {
+            System.out.println("Saldo insuficiente para completar a transação");
+        } else {
+            this.saldo -= valor;
+        }
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+}
