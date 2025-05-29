63df21691adbbe0e211ee05f6c825e5e70cf12a0
dynorph <erickmsilva19@gmail.com>
2025-05-29 20:29:38 -0300

    fix: eda: code refactoring and commenting

--- eda/search/binary_search.cpp
+++ eda/search/binary_search.cpp
@@ -1,50 +1,56 @@
-// Algoritmo que usa busca binária para encontrar um elemento numa lista ordenada
-#include <iostream>
+// algoritmo de busca binária para encontrar elementos num array ordenado
 
+#include <iostream>
 #define SIZE 10
 
 using namespace std;
 
-void printArray(int arr[]) {
+void printArray(int array[]) {
     cout << "[";
     for(int i = 0; i < SIZE; i++) {
         if(i == SIZE-1) {
-            cout << arr[i] << "]" << endl;
+            cout << array[i] << "]" << endl;
         }
         else {
-            cout << arr[i] << ", ";
+            cout << array[i] << ", ";
         }
     }
 }
 
-int binarySearch(int arr[], int alvo) {
-    int left = 0, right = SIZE-1, mid, achou = 0;
-    while(left <= right) {
-        mid = (left+right)/2;
-        if(arr[mid] == alvo) {
-            return mid;
-        }
-        else if (arr[mid] < alvo) {
-            left = mid+1;
-        }
-        else {
-            right = mid-1;
+// algoritmo de busca que consiste em divisões sucessivas de um array para encontrar um elemento específico
+bool binarySearch(int array[], int alvo) {
+    int esq = 0, dir = SIZE-1, mid;
+    // loop while repete o processo até o número ser encontrado ou não houver mais elementos para analisar
+    while(esq <= dir) {
+        mid = (esq+dir)/2;
+
+        if(array[mid] == alvo) { 
+            // se o número for encontrado, seu índice é exibido no console e a função retorna true
+            cout << "Número encontrado! Índice: " << mid+1 << endl;
+            return true;
+        } else if (array[mid] < alvo) {
+            // desconsidera a parte esquerda do array, devido ao alvo ser maior que o valor do meio, e refaz a busca
+            esq = mid+1;
+        } else {
+            // desconsidera a parte direita do array, devido ao alvo ser menor que o valor do meio, e refaz a busca
+            dir = mid-1;
         }
     }
-    return -1;
+    // retorna false se o número não for encontrado
+    return false; 
 }
 
 int main() {
-    int arr[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, alvo, resultado;
-    printArray(arr);
-    cout << "Insira o numero que deseja buscar na lista: ";
-    cin >> alvo;
-    resultado = binarySearch(arr,alvo);
-    if(resultado == -1) {
-        cout << "Numero nao encontrado" << endl;
-    }
-    else {
-        cout << "Numero encontrado! Indice: " << resultado+1 << endl;
-    }
+    int array[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, alvo, resultado;
+    // exibição do array inicial no console
+    printArray(array); 
+    // definição de um número a ser buscado pela função binarySearch
+    cout << "Insira o numero que deseja buscar no array: ";
+    cin >> alvo; 
+    // execução da função binarySearch para buscar o alvo no array
+    resultado = binarySearch(array,alvo);
+    // exibe uma mensagem de erro na tela caso o número não tenha sido encontrado
+    !(resultado) && cout << "Número não encontrado" << endl;
+    
     return 0;
 }
--- eda/search/linearsearch.cpp
+++ eda/search/linearsearch.cpp
@@ -1,6 +1,6 @@
-// Algoritmo que usa da busca sequencial para encontrar um número informado pelo usuário em uma lista não-organizada
-#include <iostream>
+// algoritmo de busca linear / sequencial para encontrar elementos em arrays não ordenados
 
+#include <iostream>
 #define SIZE 10
 
 using namespace std;
@@ -17,27 +17,31 @@ void printArray(int arr[]) {
     }
 }
 
-int linearSearch(int arr[], int alvo) {
+bool linearSearch(int arr[], int alvo) {
     int i = 0;
-    while(i < SIZE) {
+    // loop for que percorre o array até o número ser encontrado ou até não houver mais elementos para analisar
+    for(int i = 0; i < SIZE; i++) {
         if(arr[i] == alvo) {
-            return i;
-        }
-        else {
-            i++;
+            // exibe o índice do número e retorna true se ele for encontrado no array
+            cout << "Número encontrado! Índice: " << i+1 << endl;
+            return true;
         }
     }
-    return -1;
+    // retorna false se o número não for encontrado no array
+    return false;
 }
 
 int main() {
-    int alvo, i, arr[SIZE] = {72, 39, 8, 56, 91, 23, 65, 4, 19, 88};
-    printArray(arr);
+    int alvo, resultado, array[SIZE] = {72, 39, 8, 56, 91, 23, 65, 4, 19, 88};
+    // exibição do array
+    printArray(array);
+    // escolha de um número alvo para a busca
     cout << "Insira um numero para buscar: ";
     cin >> alvo;
-    switch(i = linearSearch(arr, alvo)) {
-        case -1: cout << "Numero nao encontrado\n"; break;
-        default: cout << "Numero encontrado! Indice: " << i << endl; break;
-    }
+    // busca pelo número alvo no array e armazena o resultado na busca numa variável
+    resultado = linearSearch(array,alvo);
+    // se o número não for encontrado no array, uma mensagem de erro é exibida no console
+    !(resultado) && cout << "Número não encontrado" << endl;
+    
     return 0;
 }
